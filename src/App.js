c225489e8367614a1cf1e306a5d304cf75f2bd20
hoedlto <77342371+hoedlto@users.noreply.github.com>
2022-03-27 18:08:02 -0300

    Rmv ref to js file in orig repo thats not avail

--- src/App.js
+++ src/App.js
@@ -1,50 +1,53 @@
-
-import formJSON from './formElement.json';
-import { useState, useEffect } from 'react';
-import Element from './components/Element';
-import { FormContext } from './FormContext';
+import formJSON from "./formElement.json";
+import { useState, useEffect } from "react";
+import Element from "./components/Element";
+import { FormContext } from "./FormContext";
 function App() {
   const [elements, setElements] = useState(null);
   useEffect(() => {
-    setElements(formJSON[0])
-
-  }, [])
-  const { fields, page_label } = elements ?? {}
+    setElements(formJSON[0]);
+  }, []);
+  const { fields, page_label } = elements ?? {};
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    console.log(elements)
-  }
+    console.log(elements);
+  };
   const handleChange = (id, event) => {
-    const newElements = { ...elements }
-    newElements.fields.forEach(field => {
+    const newElements = { ...elements };
+    newElements.fields.forEach((field) => {
       const { field_type, field_id } = field;
       if (id === field_id) {
         switch (field_type) {
-          case 'checkbox':
-            field['field_value'] = event.target.checked;
+          case "checkbox":
+            field["field_value"] = event.target.checked;
             break;
 
           default:
-            field['field_value'] = event.target.value;
+            field["field_value"] = event.target.value;
             break;
         }
-
-
       }
-      setElements(newElements)
+      setElements(newElements);
     });
-    console.log(elements)
-  }
+    console.log(elements);
+  };
   return (
     <FormContext.Provider value={{ handleChange }}>
       <div className="App container">
         <h3>{page_label}</h3>
         <form>
-          {fields ? fields.map((field, i) => <Element key={i} field={field} />) : null}
-          <button type="submit" className="btn btn-primary" onClick={(e) => handleSubmit(e)}>Submit</button>
+          {fields
+            ? fields.map((field, i) => <Element key={i} field={field} />)
+            : null}
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Submit
+          </button>
         </form>
-
       </div>
     </FormContext.Provider>
   );
